7411caf30b0df1bf9d20a17b8b415cfd9df68c92
SupaProgramer <dania.swirdov@gmail.com>
2020-03-06 13:09:06 +0300

    добавлен таймер

--- landing-page/js/script.js
+++ landing-page/js/script.js
@@ -33,4 +33,44 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    let deadLine = '2020-03-06 13:06:10';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / 3600 / 1000);
+            return {
+                'total':t,
+                'hours':hours,
+                'minutes':minutes,
+                'seconds':seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            if(t.total > 0) {
+                hours.textContent = (t.hours >= 10)?t.hours:'0' + t.hours;
+                minutes.textContent = (t.minutes >= 10)?t.minutes:'0' + t.minutes;
+                seconds.textContent = (t.seconds >= 10)?t.seconds:'0' + t.seconds;
+            }
+            else {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock('timer',deadLine);
 });
