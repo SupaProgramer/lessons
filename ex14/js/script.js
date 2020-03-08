f095e4d2ca1994cdec0dcea35a4abe7aa3415f27
SupaProgramer <dania.swirdov@gmail.com>
2020-03-08 19:06:09 +0300

    Добавлены промисы

---
+++ ex14/js/script.js
@@ -0,0 +1,30 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+        let request = new XMLHttpRequest();
+
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+        let promise = new Promise((resolve, reject) => {
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState === 4) {
+                    if(request.status == 200) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                }
+            });
+    });
+    promise
+            .then(() => {
+                let data = JSON.parse(request.response);
+                inputUsd.value = inputRub.value / data.usd;
+            })
+            .catch(() => {
+                inputUsd.value = "Что-то пошло не так!";
+            });
+});
